088cb4cbdc170c182828fd9151be2f51a8905599
tarekyehia7 <eng.tarekyehia7@yahoo.com>
2018-04-12 16:47:17 +0200

    adding home and about pages to the components

---
+++ src/components/about/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+    return(){
+        <div>
+            <h1>about</h1>
+        </div>
+    }
+};
+
+export default AboutPage;
