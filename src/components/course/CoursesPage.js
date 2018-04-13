c90b66acc9cefab2913baee3e0d2b9778fcc4c13
tarekyehia7 <eng.tarekyehia7@yahoo.com>
2018-04-13 12:08:49 +0200

    adding Courses Page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+class CoursesPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Courses</p>
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
