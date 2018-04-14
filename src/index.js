2c7f2aeeee82b940dca131f817fc7c1c9a7e59c0
tarekyehia7 <eng.tarekyehia7@yahoo.com>
2018-04-14 15:16:14 +0200

    handle adding new courses

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import store from './store/configureStore';
-import { Provider } from 'react-redux'
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import these css files
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store = configureStore();
 
 render(
     <Provider store={store} >
