d3c5f21159ea6e9fe6f6325b267af12a081c44d3
tarekyehia7 <eng.tarekyehia7@yahoo.com>
2018-04-15 13:13:35 +0200

    Error Handling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { loadAuthors } from './actions/authorAction';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import these css files
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
